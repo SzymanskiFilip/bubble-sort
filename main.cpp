856534daafe184444c92bbd5358b7c8384c090c9
Filip <szymanski.fp@gmail.com>
2022-04-05 22:38:31 +0200

    finished

--- main.cpp
+++ main.cpp
@@ -19,7 +19,19 @@ int main() {
         cin >> numbers[i];
     }
 
+    for(int j = 0; j < number - 1; j++){
+        for(int i = 0; i < number - 1; i++){
+            if(numbers[i] > numbers[i+1]){
+                int temp = numbers[i+1];
+                numbers[i+1] = numbers[i];
+                numbers[i] = temp;
+            }
+        }
+    }
 
+    for(int i = 0; i < number; i++){
+        cout << numbers[i] << endl;
+    }
 
     return 0;
 }
